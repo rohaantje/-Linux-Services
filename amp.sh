1e37e94d71558a6e3d15afac4d05c68e2789c72e
Ruben Rohaan <rubenrohaan@gmail.com>
2017-11-14 09:37:53 +0100

    PHP5 missing and expanded startup.sh

--- amp.sh
+++ amp.sh
@@ -4,6 +4,10 @@ echo -e "##Installing apache2##"
 logger installing apache2
 sudo apt -y install apache2
 
+echo -e "##Installing PHP5##"
+logger installing PHP5
+sudo apt -y install libapache2-mod-php5 php5 php5-common php5-curl php5-dev php5-gd php5-idn php-pear php5-imagick php5-mcrypt php5-mysql php5-ps php5-pspell php5-recode php5-xsl
+
 echo -e "##Installing PHP7##"
 logger installing PHP7
 sudo apt -y install php7.0 php7.0-cli php7.0-snmp php7.0-xml php7.0-mbstring php7.0-json php7.0-gd php7.0-gmp php7.0-zip php7.0-ldap php7.0-mcrypt
--- startup.sh
+++ startup.sh
@@ -4,3 +4,6 @@ echo -e "##Setting hostname in hosts file##"
 logger setting hostname in hosts file
 sed -i '/localhost/a 127.0.1.1 ruben-test' /etc/hosts
 
+bash amp.sh
+bash salt.sh
+bash mon.sh
