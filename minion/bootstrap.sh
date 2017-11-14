e58f12bd1e10faada2e7174b4cbdeb3974610408
Ruben Rohaan <rubenrohaan@gmail.com>
2017-11-14 16:28:20 +0100

    changed bootstraps remove not used files move state folder

--- minion/bootstrap.sh
+++ minion/bootstrap.sh
@@ -9,7 +9,10 @@ sed -i '/localhost/a 127.0.1.1 '$(hostname)'' /etc/hosts
 echo -e "##clone git and enter dir##"
 logger clone git and enter dir
 git clone https://github.com/rohaantje/-Linux-Services.git
-cd ./-Linux-Services/minion
+cd ./-Linux-Services
+rmdir /moma
+rmdir /state
+cd /minion
 
 echo -e "##running install scripts##"
 logger running install scripts
