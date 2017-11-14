c3bc8d8ccb1b8ed5dbf953db5d8172687e1555ed
Ruben Rohaan <rubenrohaan@gmail.com>
2017-11-14 15:36:42 +0100

    Salt setup

--- minion/salt.sh
+++ minion/salt.sh
@@ -3,4 +3,10 @@
 
 echo -e "##Installing SaltStack##"
 logger installing saltstack
-sudo apt salt-minion
+wget -O bootstrap-salt.sh https://bootstrap.saltstack.com
+sudo sh bootstrap-salt.sh
+
+echo -e "##set master ip##"
+logger set master ip
+sed -i "s/\#master: salt/master: 10.4.1.25/g"  /etc/salt/minion
+
--- moma/salt.sh
+++ moma/salt.sh
@@ -7,3 +7,5 @@ wget -O bootstrap-salt.sh https://bootstrap.saltstack.com
 sudo sh bootstrap-salt.sh -M
 systemctl start salt-master
 service salt-master start
+
+sed -i "s/\#Auto_accept: False/Auto_accept: true/g"  /etc/salt/master
