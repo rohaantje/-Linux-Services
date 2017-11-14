de1149bbca9cb7f75d9c25691cfe0dbf006bdcb9
Ruben Rohaan <rubenrohaan@gmail.com>
2017-11-14 09:42:51 +0100

    change dir structure

--- salt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-echo -e "##Installing SaltStack##"
-logger installing saltstack
-wget -O bootstrap-salt.sh https://bootstrap.saltstack.com
-sudo sh bootstrap-salt.sh
--- startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-echo -e "##Setting hostname in hosts file##"
-logger setting hostname in hosts file
-sed -i '/localhost/a 127.0.1.1 ruben-test' /etc/hosts
-
-bash amp.sh
-bash salt.sh
-bash mon.sh
