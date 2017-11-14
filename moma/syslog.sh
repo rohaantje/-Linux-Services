aa9c5a612efff9be617352d703dce63d9c67742a
Ruben Rohaan <rubenrohaan@gmail.com>
2017-11-14 14:31:45 +0100

    syslog changes

--- moma/syslog.sh
+++ moma/syslog.sh
@@ -2,11 +2,13 @@
 
 echo -e "##Installing syslog-ng##"
 logger installing syslog
-wget -qO - http://download.opensuse.org/repositories/home:/laszlo_budai:/syslog-ng/xUbuntu_17.04/Release.key | sudo apt-key add -
-deb http://download.opensuse.org/repositories/home:/laszlo_budai:/syslog-ng/xUbuntu_16.10 ./
-sudo apt install syslog-ng-core
+sudo apt -y install syslog-ng syslog-ng-core
 
 echo -e "##setting syslog conf file##"
 logger setting syslog conf file
 mv /etc/syslog-ng/syslog-ng.conf /etc/syslog-ng/syslog-ng.conf.bak
 cp syslog-ng.conf /etc/syslog-ng/syslog-ng.conf
+
+echo -e "##start syslog service##"
+logger start syslog service
+service syslog-ng start
