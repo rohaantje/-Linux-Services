ff2178b579a023fc3a3cb02f8b5c1f4a26288ff3
Ruben Rohaan <rubenrohaan@gmail.com>
2017-11-14 09:23:56 +0100

    mon.sh small error fix. apache dir fixed.

--- mon.sh
+++ mon.sh
@@ -20,18 +20,18 @@ apt-get install rrdtool snmp snmpd php5-snmp -y
 cd /usr/src/
 wget http://www.cacti.net/downloads/cacti-$CactiVersion.tar.gz
 tar zxvf cacti-$CactiVersion.tar.gz
-mv ./cacti-$CactiVersion/ /var/www/cacti/
+mv ./cacti-$CactiVersion/ /var/www/html/cacti/
 mysqladmin -u root -p$MySQLRootPwd create cacti
 echo "GRANT ALL ON cacti.* TO $MySQLCactiUser@localhost IDENTIFIED BY '$MySQLCactiPwd';"|mysql -u root -p$MySQLRootPwd mysql
-mysql -u $MySQLCactiUser -p$MySQLCactiPwd cacti < /var/www/cacti/cacti.sql
-cd /var/www/cacti/include/
+mysql -u $MySQLCactiUser -p$MySQLCactiPwd cacti < /var/www/html/cacti/cacti.sql
+cd /var/www/html/cacti/include/
 sed -i -e 's/username = "cactiuser"/username = "'$MySQLCactiUser'"/' config.php
-sed -i -e 's/password = "cactiuser"/password = "'$MySQLCactiPwd'"/' config.php
-useradd $SystemCactiUser -g www-data -d /var/www/cacti -s /bin/false
-chown -R $SystemCactiUser:www-data /var/www/cacti/rra/ /var/www/cacti/log/
-chmod -R 770 /var/www/cacti/rra/ /var/www/cacti/log/
+sed -i -e 's/password = "cactiuser"/password = "'$MySQLCactiPwd'"/' config.phpcd
+useradd $SystemCactiUser -g www-data -d /var/www/html/cacti -s /bin/false
+chown -R $SystemCactiUser:www-data /var/www/html/cacti/rra/ /var/www/html/cacti/log/
+chmod -R 770 /var/www/html/cacti/rra/ /var/www/html/cacti/log/
 touch /etc/cron.d/cacti
-echo "*/5 * * * * $SystemCactiUser php /var/www/cacti/poller.php >/dev/null 2>&1" > /etc/cron.d/cacti
+echo "*/5 * * * * $SystemCactiUser php /var/www/html/cacti/poller.php >/dev/null 2>&1" > /etc/cron.d/cacti
 rm -f /usr/src/cacti-$CactiVersion.tar.gz
 
 echo -e "##Installing settings plugins##"
@@ -40,7 +40,7 @@ cd /usr/src/
 wget https://docs.cacti.net/_media/plugin:settings-v$SettingsVersion.tgz
 mv plugin\:settings-v$SettingsVersion.tgz settings-v$SettingsVersion.tgz
 tar zxvf ./settings-v$SettingsVersion.tgz
-mv /usr/src/settings/ /var/www/cacti/plugins/
+mv /usr/src/settings/ /var/www/html/cacti/plugins/
 rm -f /usr/src/settings-v$SettingsVersion.tgz
 
 echo -e "##Installing syslog plugins##"
@@ -49,5 +49,5 @@ cd /usr/src/
 wget https://docs.cacti.net/_media/plugin:syslog-v$SyslogVersion.tgz
 mv plugin\:syslog-v$SyslogVersion.tgz syslog-v$SyslogVersion.tgz
 tar zxvf ./syslog-v$SyslogVersion.tgz
-mv /usr/src/syslog/ /var/www/cacti/plugins/
+mv /usr/src/syslog/ /var/www/html/cacti/plugins/
 rm -f /usr/src/syslog-v$SyslogVersion.tgz
